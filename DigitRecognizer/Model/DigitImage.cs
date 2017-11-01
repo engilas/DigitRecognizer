241cc148ddee35528394f17255915beb3c3c6b72
Alexey Rydaev <alexey.rydaev@pagru.com>
2017-11-01 10:16:36 +0300

    init

---
+++ DigitRecognizer/Model/DigitImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitRecognizer.Model
+{
+    public class DigitImage
+    {
+        // an MNIST image of a '0' thru '9' digit
+        public int width; // 28
+        public int height; // 28
+        public byte[][] pixels; // 0(white) - 255(black)
+        public byte label; // '0' - '9'
+
+        public DigitImage(int width, int height, byte[][] pixels, byte label)
+        {
+            this.width = width; this.height = height;
+            this.pixels = new byte[height][];
+            for (int i = 0; i < this.pixels.Length; ++i)
+                this.pixels[i] = new byte[width];
+
+            for (int i = 0; i < height; ++i)
+            for (int j = 0; j < width; ++j)
+                this.pixels[i][j] = pixels[i][j];
+
+            this.label = label;
+        }
+    }
+}
---
+++ DigitRecognizer/Model/MnistLoader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitRecognizer.Model
+{
+    public class MnistLoader
+    {
+        private readonly string _trainsFilePath;
+        private readonly string _labelsFilePath;
+
+        public MnistLoader(string trainsFilePath, string labelsFilePath)
+        {
+            _trainsFilePath = trainsFilePath;
+            _labelsFilePath = labelsFilePath;
+        }
+
+        public TrainSet[] GeTrainSets()
+        {
+            
+        }
+    
+
+        public static DigitImage[] LoadData(string pixelFile, string labelFile)
+        {
+            int numImages = 60000;
+            DigitImage[] result = new DigitImage[numImages];
+            byte[][] pixels = new byte[28][];
+            for (int i = 0; i < pixels.Length; ++i)
+                pixels[i] = new byte[28];
+            FileStream ifsPixels = new FileStream(pixelFile, FileMode.Open);
+            FileStream ifsLabels = new FileStream(labelFile, FileMode.Open);
+            BinaryReader brImages = new BinaryReader(ifsPixels);
+            BinaryReader brLabels = new BinaryReader(ifsLabels);
+            int magic1 = brImages.ReadInt32(); // обратный порядок байтов
+            magic1 = ReverseBytes(magic1); // преобразуем в формат Intel
+            int imageCount = brImages.ReadInt32();
+            imageCount = ReverseBytes(imageCount);
+            int numRows = brImages.ReadInt32();
+            numRows = ReverseBytes(numRows);
+            int numCols = brImages.ReadInt32();
+            numCols = ReverseBytes(numCols);
+            int magic2 = brLabels.ReadInt32();
+            magic2 = ReverseBytes(magic2);
+            int numLabels = brLabels.ReadInt32();
+            numLabels = ReverseBytes(numLabels);
+            for (int di = 0; di < numImages; ++di)
+            {
+                for (int i = 0; i < 28; ++i) // получаем пиксельные
+                                             // значения 28x28
+                {
+                    for (int j = 0; j < 28; ++j)
+                    {
+                        byte b = brImages.ReadByte();
+                        pixels[i][j] = b;
+                    }
+                }
+                byte lbl = brLabels.ReadByte(); // получаем маркеры
+                DigitImage dImage = new DigitImage(28, 28, pixels, lbl);
+                result[di] = dImage;
+            } // по каждому изображению
+            ifsPixels.Close(); brImages.Close();
+            ifsLabels.Close(); brLabels.Close();
+            return result;
+        }
+
+        public static int ReverseBytes(int v)
+        {
+            byte[] intAsBytes = BitConverter.GetBytes(v);
+            Array.Reverse(intAsBytes);
+            return BitConverter.ToInt32(intAsBytes, 0);
+        }
+
+        public static Bitmap MakeBitmap(DigitImage dImage, int mag)
+        {
+            int width = dImage.width * mag;
+            int height = dImage.height * mag;
+            Bitmap result = new Bitmap(width, height);
+            Graphics gr = Graphics.FromImage(result);
+            for (int i = 0; i < dImage.height; ++i)
+            {
+                for (int j = 0; j < dImage.width; ++j)
+                {
+                    int pixelColor = 255 - dImage.pixels[i][j]; // Черные цифры
+                    Color c = Color.FromArgb(pixelColor, pixelColor, pixelColor);
+                    SolidBrush sb = new SolidBrush(c);
+                    gr.FillRectangle(sb, j * mag, i * mag, mag, mag);
+                }
+            }
+            return result;
+        }
+    }
+}
